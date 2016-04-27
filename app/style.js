214b3c04e1563eab8f3094ea27ec3f67cffc7b13
GunB <heinerangarita@gmail.com>
2016-04-27 17:56:56 -0500

    Partial commit 27/04/2016 ie FIX

--- app/style.js
+++ app/style.js
@@ -21,4 +21,31 @@
         $("[toggable-element=" + attr + "]").toggleClass("inactive");
     });
 
+    $("body").on("click", ".dropdown, *", function (event) {
+        var that = this;
+        if($(that).hasClass("dropdown")){
+            $(".dropdown").removeClass("js__dropdown--open");
+            $(this).addClass("js__dropdown--open");
+            event.stopPropagation();
+        }else{
+            $(".dropdown").removeClass("js__dropdown--open");
+        }
+        //console.log(that, $(that).hasClass("dropdown"));
+        $(".dropdown").each(function (k, v) {
+            if (v !== that && !$(v).hasClass("js__dropdown--open")) {
+                $("[type=checkbox]", v).each(function () {
+                    if (!$(this).is(':checked')) {
+                        $(this).prop('checked', true).attr('checked', true);
+                    }
+                });
+            }else{
+                $("[type=checkbox]", v).removeAttr("checked");
+            }
+        });
+    });
+    
+    $("body").on("click", ".dropdown, *", function (event) {
+        //console.warn("entra");
+    });
+
 }(jQuery));
