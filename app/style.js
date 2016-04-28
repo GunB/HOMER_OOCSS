527ec55373e40bf70be831af4419ad62ff108176
GunB <heinerangarita@gmail.com>
2016-04-28 11:44:33 -0500

    Partial commit 28/04/2016 - 11:44am

--- app/style.js
+++ app/style.js
@@ -5,47 +5,55 @@
  */
 
 (function ($) {
-    /*$.fn.greenify = function (options) {
-     var that = this;
-     var attr = this.attr("toggle-element");
-     
-     if(attr){
-     $()
-     }
-     
-     return this;
-     };*/
+    var dropdown_close = function (htmlElement) {
+        $(".dropdown").each(function () {
+            if (this !== htmlElement && !$(this).hasClass("js__dropdown--open")) {
+                $("[type=checkbox]", this).each(function () {
+                    if (!$(this).is(':checked')) {
+                        $(this).prop('checked', true).attr('checked', true);
+                    }
+                });
+            } else {
+                $("[type=checkbox]", this).removeAttr("checked");
+            }
+        });
+    };
+
+    var boolean_and_array_join = function (array) {
+        var resp = !!(array.length);
+        $.each(array, function (k, v) {
+            resp = (!!v && resp);
+            if (!resp) {
+                return false;
+            }
+        });
+        return resp;
+    };
 
     $("body").on("click", "[toggle-element]", function () {
         var attr = $(this).attr("toggle-element");
         $("[toggable-element=" + attr + "]").toggleClass("inactive");
     });
 
-    $("body").on("click", ".dropdown, *", function (event) {
-        var that = this;
-        if($(that).hasClass("dropdown")){
-            $(".dropdown").removeClass("js__dropdown--open");
-            $(this).addClass("js__dropdown--open");
-            event.stopPropagation();
-        }else{
-            $(".dropdown").removeClass("js__dropdown--open");
-        }
-        //console.log(that, $(that).hasClass("dropdown"));
-        $(".dropdown").each(function (k, v) {
-            if (v !== that && !$(v).hasClass("js__dropdown--open")) {
-                $("[type=checkbox]", v).each(function () {
-                    if (!$(this).is(':checked')) {
-                        $(this).prop('checked', true).attr('checked', true);
-                    }
-                });
-            }else{
-                $("[type=checkbox]", v).removeAttr("checked");
-            }
-        });
+    $("body").on("click", ".dropdown", function (event) {
+        $(".dropdown").not(this).removeClass("js__dropdown--open");
+        $(this).toggleClass("js__dropdown--open");
+        dropdown_close();
     });
-    
-    $("body").on("click", ".dropdown, *", function (event) {
-        //console.warn("entra");
+
+    $("body").on("click", "*", function (event) {
+        if ($(".js__dropdown--open").length) {
+            var dropdown_checks = [
+                !$(event.target).closest('.dropdown').length,
+                !$(event.target).is('.dropdown')
+            ];
+            if ($(".js__dropdown--open").length &&
+                    !$(event.target).closest('.dropdown').length &&
+                    !$(event.target).is('.dropdown')) {
+                $(".dropdown").removeClass("js__dropdown--open");
+                dropdown_close();
+            }
+        }
     });
 
 }(jQuery));
